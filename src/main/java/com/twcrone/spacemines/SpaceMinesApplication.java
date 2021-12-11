572672925e124de6bafdcc93790a2dff34e0641d
Todd W Crone <twcrone@gmail.com>
2021-12-10 20:45:21 -0500

    Clean out unused stuff

---
+++ src/main/java/com/twcrone/spacemines/SpaceMinesApplication.java
@@ -0,0 +1,13 @@
+package com.twcrone.spacemines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceMinesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpaceMinesApplication.class, args);
+  }
+
+}
