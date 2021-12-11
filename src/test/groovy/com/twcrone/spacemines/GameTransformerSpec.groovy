b280ea939d37e8fccb0861442371a19bd46c819b
Todd W Crone <twcrone@gmail.com>
2021-12-11 14:10:12 -0500

    Fix Spock Groovy version

---
+++ src/test/groovy/com/twcrone/spacemines/GameTransformerSpec.groovy
@@ -0,0 +1,10 @@
+package com.twcrone.spacemines
+
+import spock.lang.Specification
+
+class GameTransformerSpec extends Specification {
+    def "Transform"() {
+        expect:
+        true
+    }
+}
